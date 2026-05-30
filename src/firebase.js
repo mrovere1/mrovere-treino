f5229681c07d10f1424df7789824161600de87a4
mrovere1 <132943052+mrovere1@users.noreply.github.com>
2026-05-30 08:26:53 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -18,12 +18,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "PASTE_HERE",
-  authDomain: "PASTE_HERE",
-  projectId: "PASTE_HERE",
-  storageBucket: "PASTE_HERE",
-  messagingSenderId: "PASTE_HERE",
-  appId: "PASTE_HERE"
+    apiKey: "AIzaSyD_ctPPEDTV8iQJ4MAwL4A2FlIHhC-ujs0",
+    authDomain: "mrovere-fire.firebaseapp.com",
+    projectId: "mrovere-fire",
+    storageBucket: "mrovere-fire.firebasestorage.app",
+    messagingSenderId: "47769290906",
+    appId: "1:47769290906:web:128d1c0956af6d526d3723",
+    measurementId: "G-8LP1NM8LVN"
 };
 
 const firebaseApp = getApps().length ? getApp() : initializeApp(firebaseConfig);
